943ff646915495d49def3087987523c0c52db570
dav <fds>
2014-05-14 20:38:00 -0300

    detecting circles

---
+++ circular-fire/fire_detector.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+# This is a standalone program. Pass an image name as a first parameter of the program.
+
+## Reference http://opencv-python-tutroals.readthedocs.org/en/latest/py_tutorials/py_imgproc/py_houghcircles/py_houghcircles.html
+
+import numpy as np
+
+import cv2
+
+
+img = cv2.imread('stuff.jpg', 0)
+img = cv2.medianBlur(img, 5)
+cimg = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
+
+circles = cv2.HoughCircles(img, cv2.cv.CV_HOUGH_GRADIENT, 1, 20,
+                           param1=50, param2=30, minRadius=10, maxRadius=500)
+
+circles = np.uint16(np.around(circles))
+for i in circles[0, :]:
+    # draw the outer circle
+    cv2.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 2)
+    # draw the center of the circle
+    cv2.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
+
+cv2.imshow('detected circles', cimg)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
