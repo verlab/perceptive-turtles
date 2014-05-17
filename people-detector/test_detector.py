525fcad5cbc817963931b1177715c9e5fd761010
dav <fds>
2014-05-17 15:56:35 -0300

    doc and format

--- people-detector/test_detector.py
+++ people-detector/test_detector.py
@@ -1,19 +1,21 @@
-import people_detector
 import cv2
-import numpy as np
+
+from people_detector import PeopleDetector
+
 
 def run():
-	detector = people_detector.PeopleDetector()
+    detector = PeopleDetector()
 
-	frame = cv2.imread("photo_0.png")
+    frame = cv2.imread("photo_0.png")
 
-	output = detector.test_image_rotate_list( frame )
+    output = detector.test_image_rotate_list(frame)
 
-	# print detector.has_person( frame )
+    # print detector.has_person( frame )
 
-	for idx, image in enumerate(output):
-		cv2.imshow("test", image)
-		cv2.waitKey(0)
+    for idx, image in enumerate(output):
+        cv2.imshow("test", image)
+        cv2.waitKey(0)
+
 
 if __name__ == "__main__":
-	run()
+    run()
