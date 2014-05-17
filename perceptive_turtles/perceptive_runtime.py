32892193a0bee6487ce67e7e21ab74654bcb3da7
dav <fds>
2014-05-17 00:53:32 -0300

    geotransform

---
+++ perceptive_turtles/perceptive_runtime.py
@@ -0,0 +1,26 @@
+
+
+
+
+## Read tiff files
+
+#for each tiff file
+    ### Detect people
+    # transform position
+    # add evidence
+
+    ### Detect fire
+    # transform position
+    # add evidence
+
+    ### show results
+    # show bayesian network
+    # write shapes for results
+    # view pie charts
+
+    ## sleep
+
+
+
+
+
