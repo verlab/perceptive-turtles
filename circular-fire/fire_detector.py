5d7f9c748597230cdbfc0a49f6eec26314ddd9e4
dav <fds>
2014-05-17 21:25:14 -0300

    docucmentation and spaces for tabs

--- circular-fire/fire_detector.py
+++ circular-fire/fire_detector.py
@@ -1,44 +1,55 @@
-import cv2
 import numpy as np
 
+import cv2
+
+
 class CircleFireDetector(object):
-  LOWER_BOUND = np.array([142,52,147])
-  UPPER_BOUND = np.array([185,255,255])
+    """
+    Detect circular paper simulating fire.
+    """
+    LOWER_BOUND = np.array([142, 52, 147])
+    UPPER_BOUND = np.array([185, 255, 255])
 
-  def __init__(self):
-    pass
+    def __init__(self):
+        pass
 
-  def test_image(self, img):
-    source = img
-    source_gray = cv2.cvtColor(source, cv2.COLOR_BGR2GRAY)
-    source_blur = cv2.medianBlur(source_gray, 7)
-    source_hsv = cv2.cvtColor(source, cv2.COLOR_BGR2HSV)
+    def test_image(self, img):
+        """
 
-    circles = cv2.HoughCircles(source_blur, cv2.HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=10, maxRadius=50)
+        :param img:
+        :return:
+        """
+        source = img
+        source_gray = cv2.cvtColor(source, cv2.COLOR_BGR2GRAY)
+        source_blur = cv2.medianBlur(source_gray, 7)
+        source_hsv = cv2.cvtColor(source, cv2.COLOR_BGR2HSV)
 
-    found_list = []
+        circles = cv2.HoughCircles(source_blur, cv2.cv.CV_HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=10,
+                                   maxRadius=50)
 
-    if circles != None:
-      circles = np.uint16(np.around(circles))
-      circles = circles[0]
-      roi_list = []
+        found_list = []
 
-      for mark in circles:
-        x1 = mark[1] - (mark[2] * np.sqrt(2)/2)
-        y1 = mark[0] - (mark[2] * np.sqrt(2)/2)
-        size = (mark[2] * np.sqrt(2)/2 * 2)
+        if circles != None:
+            circles = np.uint16(np.around(circles))
+            circles = circles[0]
+            roi_list = []
 
-        roi = np.array( source_hsv[ x1:x1+size, y1:y1+size ] )
-        roi_list.append( (roi, (mark[0], mark[1])) )
+            for mark in circles:
+                x1 = mark[1] - (mark[2] * np.sqrt(2) / 2)
+                y1 = mark[0] - (mark[2] * np.sqrt(2) / 2)
+                size = (mark[2] * np.sqrt(2) / 2 * 2)
 
-    for roi, position in roi_list:
-      if not (roi.shape[0] and roi.shape[1]):
-        continue
+                roi = np.array(source_hsv[x1:x1 + size, y1:y1 + size])
+                roi_list.append((roi, (mark[0], mark[1])))
 
-      mask = cv2.inRange(roi, self.LOWER_BOUND, self.UPPER_BOUND)
-      avg = np.average(mask)
+        for roi, position in roi_list:
+            if not (roi.shape[0] and roi.shape[1]):
+                continue
 
-      if avg > (255 * 0.8):
-        found_list.append( position )
+            mask = cv2.inRange(roi, self.LOWER_BOUND, self.UPPER_BOUND)
+            avg = np.average(mask)
 
-    return found_list
+            if avg > (255 * 0.8):
+                found_list.append(position)
+
+        return found_list
