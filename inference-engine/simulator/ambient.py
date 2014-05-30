eb30c9c2cf5e8ceed49cdbef654dec9f50adee93
dav <fds>
2014-05-30 02:28:13 -0300

    simulator for multiple tests

---
+++ inference-engine/simulator/ambient.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+
+class Ambient(object):
+    def __init__(self, n_people=10, n_fire=5, distance_danger=2, map_size=[100, 100]):
+        self.map_size = map_size
+        self.distance_danger = distance_danger
+        self.n_fire = n_fire
+        self.n_people = n_people
+
+        self.people_points = np.random.rand(n_people, 2)
+        self.points_in_map(self.people_points)
+
+
+        self.fire_points = np.random.rand(n_fire, 2)
+        self.points_in_map(self.fire_points)
+
+
+
+    def points_in_map(self, people_points):
+        """
+        Distribute points in map.
+        """
+        for i, size in enumerate(self.map_size):
+            for col in [people_points.T[i]]:
+                col *= size
+
