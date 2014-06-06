79282a14353e415c0f3e018061e9d006b1676911
Ramon Melo <workmelo@gmail.com>
2014-06-06 18:33:34 -0300

    Map plot

--- inference-engine/simulator/runtime_simulator.py
+++ inference-engine/simulator/runtime_simulator.py
@@ -2,6 +2,9 @@ from infengine.Evidence import Evidence
 from simulator.ambient import Ambient
 from simulator.sensor import Sensor
 
+import matplotlib.pyplot as plt
+import numpy as np
+
 FIRE_SENSOR = "Fire_Sensor"
 BOOLEAN_STATES = ["true", "false"]
 PEOPLE_SENSOR = "Human_Sensor"
@@ -37,6 +40,19 @@ add_evidences(true_points, false_points, PEOPLE_SENSOR)
 # add_evidences(true_points, false_points, FIRE_SENSOR)
 
 ### PLOT
+
+people = ambient.people_points.round().astype(np.int32)
+people_true = true_points.round().astype(np.int32)
+people_false = false_points.round().astype(np.int32)
+
+img = np.zeros( (ambient.map_size[0], ambient.map_size[1], 3), dtype=np.int32 )
+
+img[ people[:,0], people[:,1] ] = [100,100,100]
+img[ people_true[:,0], people_true[:,1] ] = [0,100,0]
+img[ people_false[:,0], people_false[:,1] ] = [100,0,0]
+
+plt.imshow( img )
+plt.show()
 
 ## Run engine
 
