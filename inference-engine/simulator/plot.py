18966bbb0f81f92e9bfa4fd634472226beb0dcbf
dav <fds>
2014-06-07 03:09:53 -0300

    completed for humans

---
+++ inference-engine/simulator/plot.py
@@ -0,0 +1,18 @@
+
+
+
+### PLOT
+
+# noinspection PyNoneFunctionAssignment
+# people = ambient.people_points.round().astype(np.int32)
+# people_true = true_points.round().astype(np.int32)
+# people_false = false_points.round().astype(np.int32)
+#
+# img = np.zeros((ambient.map_size[0], ambient.map_size[1], 3), dtype=np.int32)
+#
+# img[people[:, 0], people[:, 1]] = [100, 100, 100]
+# img[people_true[:, 0], people_true[:, 1]] = [0, 100, 0]
+# img[people_false[:, 0], people_false[:, 1]] = [100, 0, 0]
+#
+# plt.imshow(img)
+# plt.show()
